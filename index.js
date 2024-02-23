5e38f9910bfa7beeb428b0d3cd90b366910fa723
Adam Khodier <khodieradam1297@gmail.com>
2024-02-23 00:07:15 -0500

    while condition to swap

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 // Poor sorting alg and is mainly used as an intro to sorting algs.
 
 function bubbleSort(arr) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > arr[i-1]) {
-            let temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-        }
-    }
+  for (let i = 0; i < arr.length; i++) {
+    let swapped;
+    do {
+      swapped = false;
+      if (arr[i] > arr[i - 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    } while (swapped);
+  }
 }
+
+const array1 = [1,5,10,15,20]
+console.log(bubbleSort(array1))
